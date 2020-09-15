13501c01b6455aa7d323ba8496658957f1ed3a9b
saurabh-egov <saurabh.s@egovernments.org>
2020-09-15 21:51:21 +0530

    radio button css completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,18 +11,21 @@ module.exports = {
       lightblue: '#1D70B8',
       white: '#ffffff',
       black: '#231F20',
-      hdblack: '#0B0C0C',
+      headerblack: '#0B0C0C',
       bggrey: '#F3F2F1',
       grey: '#505A5F',
+      radioblack: '#222222',
     },
     fontFamily:{
       sans:['"Roboto Condensed"'],
     },
     fontSize:{
       navbarheader: ['20px','24px'],
-      h1: ['25px','40px'],
+      h1: ['28px','40px'],
       a: ['15px','20px'],
       pointer: ['10px', '10px'],
+      cardtext: ['16px','24px'],
+      label: ['19px', '25px'],
     },
     padding:{
       sm: '8px',
@@ -30,9 +33,11 @@ module.exports = {
       lg: '24px'
     },
     margin:{
+      radiobtnx: '7.5px',
       sm: '8px',
+      radiobtnleft: '15px',
       md: '16px',
-      lg: '24px'
+      lg: '24px',
     },
     fontWeight:{
       light: 300,
@@ -42,9 +47,20 @@ module.exports = {
       default: '1px',
       '0': '0',
       '2': '2px',
+      '10': '10px'
+    },
+    boxShadow:{
+      card: '0 1px 2px 0 rgba(0, 0, 0, 0.16)',
+      radiobtn: '0 0 0 5px rgb(29, 112, 184)',
+    },
+    inset:{
+      '0': 0,
+      '10': '10px'
     },
     extend: {},
   },
-  variants: {},
+  variants: {
+    
+  },
   plugins: [],
 }
