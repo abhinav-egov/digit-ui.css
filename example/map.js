2530c6f269b0cc69374e4cb98919088ca74ba037
saurabh-egov <saurabh.s@egovernments.org>
2020-09-24 11:32:43 +0530

    small structure management and clutter cleanup

--- example/map.js
+++ example/map.js
@@ -15,7 +15,7 @@ window.initAutocomplete = function () {
 
   const input = document.getElementById("pac-input");
   const searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // Bias the SearchBox results towards current map's viewport.
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // Bias the SearchBox results towards current map's viewport.
 
   map.addListener("bounds_changed", () => {
     searchBox.setBounds(map.getBounds());
